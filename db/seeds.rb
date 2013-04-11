c4f3f77d2bab7b4ccb000ee56eeebd5f5c77490b
Andrey Kryachkov <kryachkov.andrey@gmail.com>
2013-04-11 20:56:34 +0300

    [+] Category class

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create(name: 'Home', icon: 'icon-home')
+Category.create(name: 'Food', icon: 'icon-food')
+Category.create(name: 'Car', icon: 'icon-truck')
+Category.create(name: 'Cell', icon: 'icon-mobile-phone')
+Category.create(name: 'Internet', icon: 'icon-globe')
+Category.create(name: 'Medicine', icon: 'icon-medkit')
